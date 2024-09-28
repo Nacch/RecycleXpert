52e4efb846af1896bf2871f1f86c352f39e1b4e0
Nacch <83931811+Nacch@users.noreply.github.com>
2024-09-27 21:16:59 -0500

    Delete backend_202402 directory

--- backend_202402/src/main/java/backend/project/services/RegisterEventService.java
+++
@@ -1,8 +0,0 @@
-package backend.project.services;
-
-import backend.project.entities.RegisterEvent;
-
-public interface RegisterEventService {
-    public RegisterEvent registerVolunteerToEvent(Long eventId, Long volunteerId);
-    public void unregisterVolunteerFromEvent(Long eventId, Long volunteerId);
-}
