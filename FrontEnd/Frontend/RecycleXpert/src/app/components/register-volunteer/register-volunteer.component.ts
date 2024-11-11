ef3967434585e7c4a6346be715ade453fad9bdd8
Frank Huaman <franklhs98@gmail.com>
2024-11-11 17:33:22 -0500

    Implementación - Listado Voluntarios
    Se añadió la lista de voluntarios y los registros

--- FrontEnd/Frontend/RecycleXpert/src/app/components/register-volunteer/register-volunteer.component.ts
+++ FrontEnd/Frontend/RecycleXpert/src/app/components/register-volunteer/register-volunteer.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { VolunteerService } from '../../services/volunteer.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-volunteer',
@@ -6,5 +10,35 @@ import { Component } from '@angular/core';
   styleUrl: './register-volunteer.component.css'
 })
 export class RegisterVolunteerComponent {
+  addForm! : FormGroup;
+
+  constructor(
+    private volunteerService: VolunteerService,
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar,
+    private router: Router,
+    private activatedRoute:ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.CrearFormulario();
+  }
+
+  CrearFormulario(){
+    this.addForm = this.fb.group({
+
+
+    });
+
+  }
+  RegistrarVolunteer(): void {
+
+
+
+  }
+
+  onCancel(): void {
+    this.router.navigate(['/volunteer-listar']);
+  }
 
 }
--- FrontEnd/Frontend/RecycleXpert/src/app/components/volunteer-listar/volunteer-listar.component.ts
+++ FrontEnd/Frontend/RecycleXpert/src/app/components/volunteer-listar/volunteer-listar.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { Volunteer } from '../../models/volunteer';
+import { VolunteerService } from '../../services/volunteer.service';
 
 @Component({
   selector: 'app-volunteer-listar',
@@ -6,5 +10,48 @@ import { Component } from '@angular/core';
   styleUrl: './volunteer-listar.component.css'
 })
 export class VolunteerListarComponent {
+  cant_alquilers: number=0;
+  dsVolunteer = new MatTableDataSource<Volunteer>();
+
+  displayedColumns: string[] = ['id', 'clientName', 'rentDays', 'rentDate', 'pricePerDay', 'insurance', 'endDate', 'totalPrice', 'Acciones'];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dsVolunteer.filter = filterValue.trim().toLowerCase();
+  }
+  constructor(private volunteerService: VolunteerService) {}
+
+  ngOnInit(): void {
+    this.loadVolunteer();
+
+  }
+  ngAfterViewInit(): void {
+    this.dsVolunteer.paginator = this.paginator!;
+  }
+
+  loadVolunteer(){
+    this.volunteerService.getVolenteer().subscribe({
+      next:(data:Volunteer[]) =>{
+        this.dsVolunteer.data = data;
+        this.cant_alquilers = data.length;
+      },
+      error:(err) =>{
+        console.log(err);
+      }
+    });
+  }
+
+  borrarVoluntario(id: number){
+    this.volunteerService.deleteVolunteer(id).subscribe({
+      next: (data)=>{
+        this.loadVolunteer();
+      },
+      error:(err) =>{
+        console.log(err);
+      }
+    })
+  }
 
 }
