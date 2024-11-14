730fde7dec1182e31adf9de0755c78866a6d3d52
Frank Huaman <franklhs98@gmail.com>
2024-11-13 19:16:35 -0500

    Update de NPM

--- FrontEnd/Frontend/RecycleXpert/src/app/components/login/login.component.ts
+++ FrontEnd/Frontend/RecycleXpert/src/app/components/login/login.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  ocultarPass: boolean = true;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private snack: MatSnackBar,
+    private userService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    this.creaFormulario();
+  }
+
+  creaFormulario() {
+    this.loginForm = this.formBuilder.group({
+      userName: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  login() {
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    const user: User = {
+      id: 0,
+      userName: this.loginForm.get('userName')?.value,
+      password: this.loginForm.get('password')?.value,
+      authorities: ''
+    };
+
+    this.userService.login(user).subscribe({
+      next: (data) => {
+        this.router.navigate(['/home']);
+      },
+      error: (err) => {
+        this.snack.open('Hubo un error en la identificación del usuario', 'OK', { duration: 2000 });
+      }
+    });
+  }
+
+  cancelar() {
+    this.router.navigate(['/']);
+  }
 }
---
+++ FrontEnd/Frontend/RecycleXpert/src/app/services/user.service.ts
@@ -0,0 +1,78 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from '../models/user';
+import { tap } from 'rxjs';
+import { Token } from '../models/token';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+ 
+  ruta_servidor: string = "http://localhost:8080/api";
+  recurso:string = "users";
+
+  constructor(private http:HttpClient) { }
+
+
+  getUser(id: number){
+    return this.http.get<User>(this.ruta_servidor+"/"+this.recurso+"/"+id.toString());
+  }
+
+
+
+  newUser(user: User){
+    return this.http.post<User>(this.ruta_servidor+"/"+this.recurso + "/" + "register",user);
+  }
+
+  login(user: User){
+    this.logout();
+    return this.http.post<Token>(this.ruta_servidor+"/"+this.recurso + "/" + "login",user).pipe(
+      tap( (resultado:Token)=>{
+        localStorage.setItem('jwtToken', resultado.jwtToken);
+        localStorage.setItem('user_id', resultado.user_id.toString());
+        localStorage.setItem('authorities', resultado.authorities);
+      }
+      )
+    );
+  }
+
+  logout(){
+    if (typeof localStorage !== 'undefined') {
+      localStorage.clear();
+    }
+  }
+
+  hayUsuarioLogeado(){
+    if (this.getUserIdActual() == null || this.getUserIdActual() == "") {
+      return false;
+    }
+    return true;
+  }
+
+
+  getTokenActual() {
+    if (typeof localStorage !== 'undefined') {
+      return localStorage.getItem('jwtToken');  
+    } 
+    return null;
+  }
+
+  getUserIdActual() {
+    if (typeof localStorage !== 'undefined') {
+      return localStorage.getItem('user_id');
+    }
+    return null;
+
+  }
+
+  getAuthoritiesActual() {
+    if (typeof localStorage !== 'undefined') {
+      return localStorage.getItem('authorities');
+    }
+    return null;
+  }
+
+
+}
