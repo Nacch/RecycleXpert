6125f44fde742b4b94d6a7b9d3b08fa11b5c0a5d
Frank Huaman <franklhs98@gmail.com>
2024-11-14 10:41:42 -0500

    Organization - Service
    Implementación para el servidor

---
+++ FrontEnd/Frontend/RecycleXpert/src/app/models/organization.ts
@@ -0,0 +1,9 @@
+export interface Organization{
+
+    id: number;                  // ID único de la organización
+    organizationName: string;    // Nombre de la organización
+    orgType: string;             // Tipo de organización (por ejemplo, NGO, Non-Profit)
+    contactInfo: string;         // Información de contacto (email, teléfono, etc.)
+    collaborationArea: string;
+
+}
---
+++ FrontEnd/Frontend/RecycleXpert/src/app/services/organization.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Organization } from '../models/organization';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrganizationService {
+    // Ruta a donde se conectará
+    servidor: string = "http://localhost:8080/api";
+    recurso: string = "organizations";
+
+  constructor(private http: HttpClient) {}
+
+  getOrganizations(){
+    return this.http.get<Organization[]>(this.servidor+"/"+this.recurso);
+  }
+
+  getOrganization(id: number){
+    return this.http.get<Organization>(this.servidor+"/"+this.recurso+"/"+id.toString());
+  }
+
+ 
+}
